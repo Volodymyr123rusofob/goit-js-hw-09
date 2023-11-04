feb0c9907a7f07cdb7a6cb2683fea866779dce1c
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2023-11-05 00:02:33 +0200

    task-2-9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+let timerId = null;
+refs.btnStart.addEventListener('click', () => {
+  timerId = setInterval(onSwitch, 1000);
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+});
+refs.btnStop.addEventListener('click', offSwitch);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+function onSwitch() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
 
+function offSwitch() {
+  clearInterval(timerId);
+  refs.btnStop.disabled = true;
+  refs.btnStart.disabled = false;
+}
