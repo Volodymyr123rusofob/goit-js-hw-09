e952f228e9160b38a9cb9777d531ee12feeb8008
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2023-11-11 08:25:48 +0000

    Deploying to gh-pages from @ Volodymyr123rusofob/goit-js-hw-09@7e83aa8e6f78e51463895357a802af3488200aa2 🚀

---
+++ 03-promises.4ed5af81.js
@@ -0,0 +1,2 @@
+function e(e,t){Math.random()}document.querySelector(".form").addEventListener("submit",(t=>{setInterval(e)}));
+//# sourceMappingURL=03-promises.4ed5af81.js.map
---
+++ 03-promises.c1823efb.js
@@ -0,0 +1,2 @@
+!function(){function n(n,t){Math.random()}document.querySelector(".form").addEventListener("submit",(function(t){setInterval(n)}))}();
+//# sourceMappingURL=03-promises.c1823efb.js.map
