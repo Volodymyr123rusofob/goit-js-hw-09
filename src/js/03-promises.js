7e83aa8e6f78e51463895357a802af3488200aa2
Volodymyr123rusofob <136646970+Volodymyr123rusofob@users.noreply.github.com>
2023-11-11 10:25:24 +0200

    task-3-2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,19 @@
+// const refs {
+//   form: document.querySelector('.form'),
+//   delay: document.querySelector('input[name="delay"]'),
+//   step: document.querySelector('input[name="step"]'),
+//   amount: document.querySelector('input[name="amount"]'),
+// }
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+  setInterval(
+    createPromise
+    // передати номер промісу pоsition, викликати стільки разів скільки сказав користувач), затримка вказується користувачем;
+  );
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
